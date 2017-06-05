713a27b73cfa715597b68f9d5e7de61665814b3e
vagrant <vagrant@homestead>
2017-06-05 13:28:47 +0000

    post model save

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Log;
 
 class PostController extends Controller
 {
@@ -14,7 +15,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $log = Log::all();
+        return $log;
     }
 
     /**
