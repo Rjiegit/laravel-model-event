ccfa219b298eb3d98f834eb4c19c19042bb6a0df
vagrant <vagrant@homestead>
2017-06-05 13:04:40 +0000

    modify store method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -34,7 +35,11 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $post = new Post;
+        $post->content = $request->content;
+        return response()->json([
+            'message' => 'created !!!'
+        ]);
     }
 
     /**
